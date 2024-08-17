ee8e97c48cdb60f1440ef1603752c49fdba79166
lancervs <aatomala@espol.edu.ec>
2024-08-17 12:07:03 -0500

    small changes

---
+++ backend/app_inventario/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class AppInventariosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_inventario'
+    verbose_name = 'Inventory_manager'
