197238341a6c4f94e8d476baacbbaea05c2a95f6
lancervs <aatomala@espol.edu.ec>
2024-08-18 15:37:03 -0500

    model change

--- backend/app_inventario/models.py
+++ backend/app_inventario/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     stock = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    checked = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
